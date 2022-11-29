84d116b417402e26acc2a20647f28a1dfa2bdaeb
Béo Vậy Sao <55008986+Nevwex@users.noreply.github.com>
2022-11-29 07:54:29 +0700

    Update api.py

--- Cogs/mmbeo/api.py
+++ Cogs/mmbeo/api.py
@@ -4,7 +4,7 @@ async def dan_booru():
     while True:
       try:
         async with aiohttp.ClientSession() as sc:
-          async with sc.get(f"https://danbooru.donmai.us/posts/random.json?tags=is:nsfw+{tags}&api_key=Z56sewvSbtL6eVXYn6J4uZ7P&login=DoonG") as data:
+          async with sc.get(f"https://danbooru.donmai.us/posts/random.json?tags=is:nsfw+{tags}&api_key=") as data:
             i = await data.json()
             return i['file_url']
       except KeyError:
